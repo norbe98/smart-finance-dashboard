0bd5af1f4a4e56b43b54f6997c283c5130c8cb6e
norbe98 <szoke.norbi18@gmail.com>
2026-05-12 15:41:11 +0200

    add navbar responsiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,36 @@
+import { SquareMenu } from "lucide-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
     return (
-        <div className="flex justify-between">
+        <nav className="p-2 bg-slate-400">
 
-            <Link to="/">Home</Link>
+            <div className="flex justify-between">
 
-            <div className="flex gap-3">
-                <Link to="/display">Display</Link>
-                <Link to="/inventory">Inventory</Link>
+                <Link to="/">Home</Link>
+
+                <div className="hidden md:flex gap-3 px-3">
+                    <Link to="/display">Display</Link>
+                    <Link to="/inventory">Inventory</Link>
+                </div>
+
+                <button onClick={() => setIsOpen(prev => !prev)} className="flex md:hidden">
+                    <SquareMenu />
+                </button>
+
             </div>
 
-        </div>
+                {isOpen && 
+                <div className="md:hidden flex flex-col">
+                    <Link to="/display" className="text-center">Display</Link>
+                    <Link to="/inventory" className="text-center">Inventory</Link>
+                </div>
+                }
+
+        </nav>
     )
 }
