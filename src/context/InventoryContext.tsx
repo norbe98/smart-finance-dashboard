d5a483b90223b3c3bb9d8a7eb8e3e22f6006f794
norbe98 <szoke.norbi18@gmail.com>
2026-05-13 12:43:53 +0200

    rework context

--- src/context/InventoryContext.tsx
+++ src/context/InventoryContext.tsx
@@ -1,29 +1,54 @@
-import { createContext, useContext, useState } from "react";
-import type { CreateInventoryProps, Inventory } from "../types/types";
+import { createContext, useContext, useEffect, useState } from "react";
+import type { Transaction, CreateInventoryProps, Product } from "../types/types";
+import { formatDate } from "date-fns";
 
 const inventoryContext = createContext<CreateInventoryProps | null>(null)
 
 export default function InventoryProvider( {children}: {children: React.ReactNode}) {
 
-    const [inventory, setInventory] = useState<Inventory[]>([])
+    const [inventory, setInventory] = useState<Product[]>(() => {
+        const stored = localStorage.getItem('inventory')
+        return stored ? JSON.parse(stored) : []
+    })
 
-    function addProduct(product: Inventory) {
+    useEffect(() => {
+        localStorage.setItem('inventory', JSON.stringify(inventory))
+    }, [inventory])
+
+    const [transactions, setTransactions] = useState<Transaction[]>(() => {
+        const stored = localStorage.getItem('transactions')
+        return stored ? JSON.parse(stored) : []
+    })
+
+    useEffect(() => {
+    localStorage.setItem('transactions', JSON.stringify(transactions))
+    }, [transactions])
+
+    const rawDate = new Date()
+    const date = formatDate(rawDate, "yyyy.MM.dd.")
+    
+
+    function addProduct(product: Product) {
         setInventory(prev => [...prev, product])
     }
 
-    function addToStock(selected: string) {
-        setInventory(prev => prev.map(product => product.name === selected ? 
-            {...product, stock: product.stock + 1} : product 
+    function addTransaction(transaction: Transaction) {
+        setTransactions(prev => [...prev, transaction])
+    }
+
+    function addToStock(selected: string, quantity: number) {
+        setInventory(prev => prev.map(product => product.id === selected ? 
+            {...product, stock: product.stock + quantity} : product 
         ))
     }
-    function removeFromStock(selected: string) {
-        setInventory(prev => prev.map(product => product.name === selected ? 
-            {...product, stock: product.stock - 1} : product 
+    function removeFromStock(selected: string, quantity: number) {
+        setInventory(prev => prev.map(product => product.id === selected ? 
+            {...product, stock: product.stock - quantity} : product 
         ))
     }
 
     return (
-        <inventoryContext.Provider value={{ inventory, addProduct, addToStock, removeFromStock }}>
+        <inventoryContext.Provider value={{ inventory, addProduct, addTransaction, addToStock, removeFromStock, date }}>
         {children}
         </inventoryContext.Provider>
     )
