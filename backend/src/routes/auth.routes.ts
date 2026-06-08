0168c396c295b416b69649e008e4f54f4201bb23
norbe98 <szoke.norbi18@gmail.com>
2026-06-08 15:44:27 +0200

    setup server and database(postreSQL prisma) with routes and user

---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,4 @@
+import express from 'express'
+
+export const authRouter = express.Router()
+
---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import { authRouter } from './routes/auth.routes.js'
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+app.use(cors())
+app.use("/api/auth", authRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
