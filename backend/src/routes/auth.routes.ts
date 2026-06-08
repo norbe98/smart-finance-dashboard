428ada51e2b419f968b111826e3b409c25dfa8ea
norbe98 <szoke.norbi18@gmail.com>
2026-06-08 19:02:03 +0200

    add backend route with database for signin

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import express, { type Request, type Response } from 'express'
 import { prisma } from '../prisma.js'
 import bcrypt from 'bcrypt'
+import  jwt  from 'jsonwebtoken'
 
 export const authRouter = express.Router()
 
@@ -26,4 +27,37 @@ authRouter.post("/signup", async (req: Request, res: Response) => {
     })
 
     res.status(201).json(newUser.email)
+})
+
+authRouter.post("/signin", async (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    const existingUser = await prisma.user.findUnique({
+        where: {
+            email: email
+        }
+    })
+
+    if(!existingUser) return res.status(401).json({ message: "Email doesnt exists!"})
+
+    const isPasswordValid = await bcrypt.compare(password, existingUser.password)
+
+    if(!isPasswordValid) return res.status(402).json({ message: "Password is not correct!"})
+
+    const token = jwt.sign({
+            id: existingUser.id,
+            email: existingUser.email
+        },
+        process.env.JWT_SECRET!,
+        {
+            expiresIn: "1m"
+        }
+    )
+
+    res.status(200).json({
+        token,
+        id: existingUser.id,
+        email: existingUser.email
+    })
+
 })
